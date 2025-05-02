356193c8e27b560999a1e74918a2ac90c3e35f7e
Soufiane-Barrada <sb.soufiane.barrada@gmail.com>
2025-05-02 16:57:29 +0200

    Seeded lightgcn files

--- Baselines/Seeded LightGCN/run_wandb_sweep.sh
+++ Baselines/Seeded LightGCN/run_wandb_sweep.sh
@@ -7,9 +7,6 @@
 #SBATCH -G 1
 #SBATCH -t 48:00:00
 
-#module load cuda/12.6.0
-#conda activate /cluster/courses/cil/envs/collaborative_filtering/
-
 # wandb sweep wandb_sweep.yaml
-wandb agent soufianebarrada0-eth-z-rich/cil/o3cy1770
+wandb agent soufianebarrada0-eth-z-rich/cil/***
 
